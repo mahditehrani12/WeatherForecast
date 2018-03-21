663271eecc66f0fdac8eff08e364e316eff77d25
Mahdi Tehrani <mahditehrani12@gmail.com>
2018-03-21 17:43:27 +0330

    add anim & complete pollution Activity

--- app/src/main/java/ir/mahdiha/weatherforecast/app/ContactActivity.kt
+++ app/src/main/java/ir/mahdiha/weatherforecast/app/ContactActivity.kt
@@ -2,6 +2,9 @@ package ir.mahdiha.weatherforecast.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NavUtils
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import ir.mahdiha.weatherforecast.R
 
 class ContactActivity : AppCompatActivity()
@@ -11,5 +14,23 @@ class ContactActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        val toolbar = findViewById(R.id.activityContact_ToolBar_toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean
+    {
+        val id = item?.itemId
+        if (id == android.R.id.home)
+        {
+            NavUtils.navigateUpFromSameTask(this)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/ir/mahdiha/weatherforecast/app/PollutionActivity.java
+++ app/src/main/java/ir/mahdiha/weatherforecast/app/PollutionActivity.java
@@ -1,8 +1,15 @@
 package ir.mahdiha.weatherforecast.app;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import ir.mahdiha.weatherforecast.Forecast.PollutionApi;
@@ -23,6 +30,12 @@ public class PollutionActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pollution);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activityPollution_ToolBar_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -32,9 +45,25 @@ public class PollutionActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return true;
+    }
+
     private void loadData()
     {
         final PersianTextView pollutionData = (PersianTextView) findViewById(R.id.activityPollution_persianTextView_pollutionData);
+        final PersianTextView pollutionDate = (PersianTextView) findViewById(R.id.activityPollution_persianTextView_date);
+        final PersianTextView SO2 = (PersianTextView) findViewById(R.id.activityPollution_persianTextView_so2);
+        final PersianTextView O3 = (PersianTextView) findViewById(R.id.activityPollution_persianTextView_O3);
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.activityPollution_progressBar_progress);
 
         Retrofit pollutionRetrofit = App.getApplication().getmRetrofitPollution();
         PollutionApi pollutionApi = pollutionRetrofit.create(PollutionApi.class);
@@ -46,9 +75,22 @@ public class PollutionActivity extends AppCompatActivity
             public void onResponse(@NonNull Call<PollutionResponse> call , @NonNull Response<PollutionResponse> response)
             {
 
+                Animation animationFadeIn = AnimationUtils.loadAnimation( getApplicationContext() , R.anim.fade_in );
+                Animation animationFadeOut = AnimationUtils.loadAnimation( getApplicationContext() , R.anim.fade_out );
+
                 PollutionResponse pollutionResponse = response.body();
                 assert pollutionResponse != null;
+
+                progressBar.setVisibility(View.INVISIBLE);
+                progressBar.startAnimation(animationFadeOut);
+
+                pollutionData.setVisibility(View.VISIBLE);
+                pollutionData.startAnimation(animationFadeIn);
                 pollutionData.setText( String.valueOf (pollutionResponse.getData().getAqi()) );
+
+                pollutionDate.setText(pollutionResponse.getData().getTime().getS());
+                SO2.setText( String.valueOf(pollutionResponse.getData().getIaqi().getSo2().getV() ));
+                O3.setText( String.valueOf(pollutionResponse.getData().getIaqi().getO3().getV() ));
 
             }
 
